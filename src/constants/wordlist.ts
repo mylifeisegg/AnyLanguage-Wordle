a57765d887e54db4ae8547f714afdff0b31a2a5f
mle <39152675+mylifeisegg@users.noreply.github.com>
2022-02-14 20:41:07 +0900

    Update wordlist.ts

--- src/constants/wordlist.ts
+++ src/constants/wordlist.ts
@@ -1,4 +1,4 @@
-import { CONFIG } from './config'
+// import { CONFIG } from './config'
 
 export const WORDS = [
   'ㅍㅣㅇㅏㄴㅗ',
@@ -21,17 +21,17 @@ export const WORDS = [
     'ㅇㅕㄱㅜㅓㄴ',
 ]
 
-if (CONFIG.normalization) {
-  WORDS.forEach((val, i) => (WORDS[i] = val.normalize(CONFIG.normalization)))
-}
+// if (CONFIG.normalization) {
+//   WORDS.forEach((val, i) => (WORDS[i] = val.normalize(CONFIG.normalization)))
+// }
 
-function shuffle(array: any[]) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1))
-    ;[array[i], array[j]] = [array[j], array[i]]
-  }
-}
+// function shuffle(array: any[]) {
+//   for (let i = array.length - 1; i > 0; i--) {
+//     const j = Math.floor(Math.random() * (i + 1))
+//     ;[array[i], array[j]] = [array[j], array[i]]
+//   }
+// }
 
-if (CONFIG.shuffle) {
-  shuffle(WORDS)
-}
+// if (CONFIG.shuffle) {
+//   shuffle(WORDS)
+// }
